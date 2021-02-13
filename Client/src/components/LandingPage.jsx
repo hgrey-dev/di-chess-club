6e1dca0dd5a845a1ee333a7d9703d64ffb85f15d
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-02-14 12:14:59 +1300

    splash done

--- Client/src/components/LandingPage.jsx
+++ Client/src/components/LandingPage.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import NavBar from './NavBar.jsx';
 import '../stylesheets/LandingPage.css';
 
+import Logo from '../svg/DICC-logo-black.svg';
+
 const LandingPage = () => {
   return (
     <div className='LP-container'>
       <NavBar />
+      <div className='LP-splash'>
+        <div className='LP-splash-container'>
+          <img className='LP-splash-logo' src={Logo} alt='Developers Institute Chess Club' />
+          <h1 className='LP-splash-header'>Developers Institute Chess Club</h1>
+        </div>
+      </div>
     </div>
   );
 }
