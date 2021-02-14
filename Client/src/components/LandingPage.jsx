259b7990c414f6a281bc418244825da0588332a1
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-02-14 16:10:33 +1300

    Added latest club activities section

--- Client/src/components/LandingPage.jsx
+++ Client/src/components/LandingPage.jsx
@@ -8,12 +8,23 @@ const LandingPage = () => {
   return (
     <div className='LP-container'>
       <NavBar />
-      <div className='LP-splash'>
+      <section className='LP-splash'>
         <div className='LP-splash-container'>
           <img className='LP-splash-logo' src={Logo} alt='Developers Institute Chess Club' />
           <h1 className='LP-splash-header'>Developers Institute Chess Club</h1>
         </div>
-      </div>
+      </section>
+      <section className='LP-activities'>
+        <h2>Latest Club Activities</h2>
+        <div className='LP-latest'>
+          <div className='LP-latest-game'>
+            <h3>Latest Game</h3>
+          </div>
+          <div className='LP-latest-schedule'>
+            <h3>Club Activities Schedule</h3>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
